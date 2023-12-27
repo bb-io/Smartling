976602c07771a8107d0294314d30f2fce3c758cb
bohdanm-bb <bohdan.makarenko@blackbird.io>
2023-12-27 03:51:11 +0200

    New files

--- Apps.Smartling/Models/FileWrapper.cs
+++ Apps.Smartling/Models/FileWrapper.cs
@@ -1,8 +1,8 @@
-﻿using File = Blackbird.Applications.Sdk.Common.Files.File;
-    
+﻿using Blackbird.Applications.Sdk.Common.Files;
+
 namespace Apps.Smartling.Models;
 
 public class FileWrapper
 {
-    public File File { get; set; }
+    public FileReference File { get; set; }
 }
