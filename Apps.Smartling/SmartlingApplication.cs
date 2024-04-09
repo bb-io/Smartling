8048bbcbc7ad622bd5b4407208194f92c7b55c01
bZverok <137277669+bZverok@users.noreply.github.com>
2024-04-09 15:41:46 +0300

    added categories

--- Apps.Smartling/SmartlingApplication.cs
+++ Apps.Smartling/SmartlingApplication.cs
@@ -1,9 +1,16 @@
 ﻿using Blackbird.Applications.Sdk.Common;
+using Blackbird.Applications.Sdk.Common.Metadata;
 
 namespace Apps.Smartling;
 
-public class SmartlingApplication : IApplication
+public class SmartlingApplication : IApplication, ICategoryProvider
 {
+    public IEnumerable<ApplicationCategory> Categories
+    {
+        get => [ApplicationCategory.CatAndTms];
+        set { }
+    }
+    
     public string Name
     {
         get => "Smartling";
