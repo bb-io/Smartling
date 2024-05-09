3b3aa6dabcdc65cedc36a60a660c2bd86eb3f393
Cecilia Nistal <cecilia.nistal@centific.com>
2024-05-08 22:06:28 -0300

    Added List strings in job action

---
+++ Apps.Smartling/Models/Dtos/Strings/StringHashcodeLocaleDto.cs
@@ -0,0 +1,17 @@
+﻿using Blackbird.Applications.Sdk.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Apps.Smartling.Models.Dtos.Strings
+{
+    public class StringHashcodeLocaleDto
+    {
+        public string Hashcode { get; set; }
+
+        [Display("Target locale ID")]
+        public string TargetLocaleId { get; set; }
+    }
+}
