24d7986be15eadbe1d57425b4d1707b61147d1a2
vitalii-bezuhlyi <vitalii.bezuhlyi@blackbird.io>
2024-10-21 15:56:38 +0300

    Added pagination to job data handler

--- Apps.Smartling/Callbacks/CallbackList.cs
+++ Apps.Smartling/Callbacks/CallbackList.cs
@@ -2,6 +2,7 @@
 using Apps.Smartling.Callbacks.Handlers;
 using Apps.Smartling.Callbacks.Models.Payload.Jobs;
 using Apps.Smartling.Callbacks.Models.Payload.Strings;
+using Apps.Smartling.Models.Identifiers;
 using Blackbird.Applications.Sdk.Common.Webhooks;
 using Newtonsoft.Json;
 
@@ -12,37 +13,91 @@ public class CallbackList
 {
     #region Bridge callbacks
 
-    [Webhook("On job completed", typeof(JobCompletedCallbackHandler), 
+    [Webhook("On job completed", typeof(JobCompletedCallbackHandler),
         Description = "This event is triggered when a job is completed.")]
-    public Task<WebhookResponse<JobCompletedPayload>> OnJobCompleted(WebhookRequest request) 
-        => HandleCallback<JobCompletedPayload>(request);
-    
-    [Webhook("On job cancelled", typeof(JobCancelledCallbackHandler), 
+    public async Task<WebhookResponse<JobCompletedPayload>> OnJobCompleted(WebhookRequest request,
+        [WebhookParameter] JobOptionalIdentifier jobOptionalRequest)
+    {
+        var result = await HandleCallback<JobCompletedPayload>(request);
+        if (jobOptionalRequest.TranslationJobUid != null && jobOptionalRequest.TranslationJobUid == result.Result?.TranslationJobUid)
+        {
+            return GetPreflightResponse<JobCompletedPayload>();
+        }
+        
+        return result;
+    }
+
+    [Webhook("On job cancelled", typeof(JobCancelledCallbackHandler),
         Description = "This event is triggered when a job is cancelled.")]
-    public Task<WebhookResponse<JobCancelledPayload>> OnJobCancelled(WebhookRequest request) 
-        => HandleCallback<JobCancelledPayload>(request);
+    public async Task<WebhookResponse<JobCancelledPayload>> OnJobCancelled(WebhookRequest request,
+        [WebhookParameter] JobOptionalIdentifier jobOptionalRequest)
+    {
+        var result = await HandleCallback<JobCancelledPayload>(request);
+        if (jobOptionalRequest.TranslationJobUid != null && jobOptionalRequest.TranslationJobUid == result.Result?.TranslationJobUid)
+        {
+            return GetPreflightResponse<JobCancelledPayload>();
+        }
+        
+        return result;
+    }
 
     [Webhook("On string translation published", typeof(StringPublishedCallbackHandler),
         Description = "This event is triggered when a string translation is published for a locale.")]
-    public Task<WebhookResponse<StringPublishedPayload>> OnStringPublished(WebhookRequest request)
-        => HandleCallback<StringPublishedPayload>(request);
+    public async Task<WebhookResponse<StringPublishedPayload>> OnStringPublished(WebhookRequest request,
+        [WebhookParameter] StringOptionalIdentifier optionalIdentifier)
+    {
+        var result = await HandleCallback<StringPublishedPayload>(request);
+        if (optionalIdentifier.Hashcode != null && optionalIdentifier.Hashcode == result.Result?.Hashcode)
+        {
+            return GetPreflightResponse<StringPublishedPayload>();
+        }
+        
+        return result;
+    }
 
     #endregion
 
     #region Manual callbacks
 
     [Webhook("On job completed (manual)", Description = "This manual event is triggered when a job is completed.")]
-    public Task<WebhookResponse<JobCompletedPayload>> OnJobCompletedManual(WebhookRequest request) 
-        => HandleCallback<JobCompletedPayload>(request);
-    
+    public async Task<WebhookResponse<JobCompletedPayload>> OnJobCompletedManual(WebhookRequest request,
+        [WebhookParameter] JobOptionalIdentifier jobOptionalRequest)
+    {
+        var result = await HandleCallback<JobCompletedPayload>(request);
+        if (jobOptionalRequest.TranslationJobUid != null && jobOptionalRequest.TranslationJobUid == result.Result?.TranslationJobUid)
+        {
+            return GetPreflightResponse<JobCompletedPayload>();
+        }
+        
+        return result;
+    }
+
     [Webhook("On job cancelled (manual)", Description = "This manual event is triggered when a job is cancelled.")]
-    public Task<WebhookResponse<JobCancelledPayload>> OnJobCancelledManual(WebhookRequest request) 
-        => HandleCallback<JobCancelledPayload>(request);
-    
-    [Webhook("On string translation published (manual)", 
+    public async Task<WebhookResponse<JobCancelledPayload>> OnJobCancelledManual(WebhookRequest request,
+        [WebhookParameter] JobOptionalIdentifier jobOptionalRequest)
+    {
+        var result = await HandleCallback<JobCancelledPayload>(request);
+        if (jobOptionalRequest.TranslationJobUid != null && jobOptionalRequest.TranslationJobUid == result.Result?.TranslationJobUid)
+        {
+            return GetPreflightResponse<JobCancelledPayload>();
+        }
+        
+        return result;
+    }
+
+    [Webhook("On string translation published (manual)",
         Description = "This manual event is triggered when a string translation is published for a locale.")]
-    public Task<WebhookResponse<StringPublishedPayload>> OnStringPublishedManual(WebhookRequest request)
-        => HandleCallback<StringPublishedPayload>(request);
+    public async Task<WebhookResponse<StringPublishedPayload>> OnStringPublishedManual(WebhookRequest request,
+        [WebhookParameter] StringOptionalIdentifier optionalIdentifier)
+    {
+        var result = await HandleCallback<StringPublishedPayload>(request);
+        if (optionalIdentifier.Hashcode != null && optionalIdentifier.Hashcode == result.Result?.Hashcode)
+        {
+            return GetPreflightResponse<StringPublishedPayload>();
+        }
+        
+        return result;
+    }
 
     #endregion
 
@@ -56,5 +111,15 @@ public class CallbackList
             HttpResponseMessage = new HttpResponseMessage(statusCode: HttpStatusCode.OK),
             Result = payload
         });
+    }
+    
+    private WebhookResponse<T> GetPreflightResponse<T>() where T : class
+    {
+        return new WebhookResponse<T>
+        {
+            HttpResponseMessage = new HttpResponseMessage(statusCode: HttpStatusCode.OK),
+            Result = null,
+            ReceivedWebhookRequestType = WebhookRequestType.Preflight
+        };
     }
 }
---
+++ Apps.Smartling/Models/Identifiers/StringOptionalIdentifier.cs
@@ -0,0 +1,6 @@
+﻿namespace Apps.Smartling.Models.Identifiers;
+
+public class StringOptionalIdentifier
+{
+    public string? Hashcode { get; set; }
+}
