df798f255550727d0ed4218b096ab1af119ab2f8
RiabushenkoA <ariabushenko@blackbird.io>
2025-04-01 11:20:19 +0300

    Added polling on complete job

---
+++ Apps.Smartling/Polling/PollingList.cs
@@ -0,0 +1,84 @@
+﻿using Apps.Smartling.Api;
+using Apps.Smartling.Polling.Models;
+using Blackbird.Applications.Sdk.Common.Invocation;
+using Blackbird.Applications.Sdk.Common.Polling;
+using RestSharp;
+
+namespace Apps.Smartling.Polling
+{
+    [PollingEventList]
+    public class PollingList : SmartlingInvocable
+    {
+        public PollingList(InvocationContext invocationContext) : base(invocationContext)
+        {
+
+        }
+
+        [PollingEvent("On job completed [Polling]")]
+        public async Task<PollingEventResponse<DateMemory, ListJobsResponse>> OnJobCompleted(PollingEventRequest<DateMemory> request)
+        {
+            var endpoint = $"/jobs-api/v2/projects/{ProjectId}/jobs";
+
+            var jobs = (await Client.Paginate<Apps.Smartling.Polling.Models.JobItem>(
+                new SmartlingRequest(endpoint, Method.Get)
+            )).ToArray();
+
+            if (jobs.Length == 0)
+            {
+                return new PollingEventResponse<DateMemory, ListJobsResponse>
+                {
+                    FlyBird = false,
+                    Memory = request.Memory ?? new DateMemory { LastInteractionDate = DateTime.UtcNow }
+                };
+            }
+
+            if (request.Memory == null)
+            {
+                var maxCreatedAt = jobs.Max(j => j.CreatedDate);
+                var memory = new DateMemory { LastInteractionDate = maxCreatedAt };
+                return new PollingEventResponse<DateMemory, ListJobsResponse>
+                {
+                    FlyBird = false,
+                    Memory = memory
+                };
+            }
+
+            var completedJobs = jobs.Where(j => j.JobStatus == "COMPLETED" && j.CreatedDate > request.Memory.LastInteractionDate)
+                                    .ToArray();
+
+            if (completedJobs.Any())
+            {
+                var maxCreatedAt = completedJobs.Max(j => j.CreatedDate);
+                request.Memory.LastInteractionDate = maxCreatedAt;
+
+                var listJobsResponse = new Apps.Smartling.Polling.Models.ListJobsResponse
+                {
+                    Response = new Apps.Smartling.Polling.Models.ResponseData
+                    {
+                        Code = "SUCCESS",
+                        Data = new Apps.Smartling.Polling.Models.DataContent
+                        {
+                            Items = completedJobs.ToList(),
+                            TotalCount = completedJobs.Length
+                        }
+                    }
+                };
+
+                return new PollingEventResponse<DateMemory, ListJobsResponse>
+                {
+                    FlyBird = true,
+                    Memory = request.Memory,
+                    Result = listJobsResponse
+                };
+            }
+            else
+            {
+                return new PollingEventResponse<DateMemory, ListJobsResponse>
+                {
+                    FlyBird = false,
+                    Memory = request.Memory
+                };
+            }
+        }
+    }
+}
---
+++ Tests.Smartling/PollingTests.cs
@@ -0,0 +1,33 @@
+﻿using Apps.Smartling.Polling;
+using Apps.Smartling.Polling.Models;
+using Blackbird.Applications.Sdk.Common.Polling;
+using SmartlingTests.Base;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests.Smartling
+{
+    [TestClass]
+    public class PollingTests : TestBase
+    {
+        [TestMethod]
+        public async Task OnJobCompleted()
+        {
+            var customDate = DateTime.ParseExact("Mar 27, 2025, 6:31 PM", "MMM dd, yyyy, h:mm tt", CultureInfo.InvariantCulture);
+
+            var action = new PollingList(InvocationContext);
+            var request = new PollingEventRequest<DateMemory>
+            {
+                Memory = new DateMemory { LastInteractionDate = customDate }
+            };
+
+            var response = await action.OnJobCompleted(request);
+            Console.WriteLine($"{response.Result.Response.Data}");
+            Assert.IsNotNull(response);
+        }
+    }
+}
