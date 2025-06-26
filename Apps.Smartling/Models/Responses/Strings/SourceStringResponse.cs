699190b064e2fcf038ac5f6e4e35f3fa8cd7ceef
ce-nistal <cecilia.nistal@blackbird.io>
2025-06-26 15:59:07 -0300

    Flatten Content file string instructions for Get source string by hashcode action

---
+++ Apps.Smartling/Models/Responses/Strings/SourceStringResponse.cs
@@ -0,0 +1,36 @@
+﻿using Apps.Smartling.Models.Dtos.Strings;
+using Blackbird.Applications.Sdk.Common;
+using Newtonsoft.Json;
+
+namespace Apps.Smartling.Models.Responses.Strings;
+
+public class SourceStringResponse
+{
+    [Display("String instructions")]
+    public IEnumerable<string> StringInstructions { get; set; }
+
+    [Display("String variant")]
+    public string? StringVariant { get; set; }
+
+    [Display("Maximum character length")]
+    public int? MaxLength { get; set; }
+
+    [Display("Content file string instruction")]
+    public string? ContentFileInstructions { get; set; }
+
+    public SourceStringResponse(SourceStringDto dto)
+    {
+        StringInstructions = dto.StringInstructions;
+        StringVariant = dto.StringVariant;
+        MaxLength = dto.MaxLength;
+        ContentFileInstructions = dto.contentFileInstructions != null
+            ? string.Join(" ",
+                dto.contentFileInstructions
+                   .Where(i => !string.IsNullOrWhiteSpace(i.Instruction))
+                   .Select(i => $"{i.Instruction} ({i.FileUri})"))
+            : "";
+    }
+
+    public SourceStringResponse() { }
+
+}
