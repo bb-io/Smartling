49d5f58be4e261863c11fe64a1ec005a24cbdda4
RiabushenkoA <ariabushenko@blackbird.io>
2025-10-30 20:14:56 +0200

    Added new action 'Search files'

---
+++ Apps.Smartling/Models/Responses/Files/SearchFilesResponse.cs
@@ -0,0 +1,14 @@
+﻿using Apps.Smartling.Models.Requests.Files;
+using Blackbird.Applications.Sdk.Common;
+
+namespace Apps.Smartling.Models.Responses.Files
+{
+    public class SearchFilesResponse
+    {
+        [Display("Items")]
+        public IEnumerable<SourceFileSummaryDto> Items { get; set; } = Array.Empty<SourceFileSummaryDto>();
+
+        [Display("Total count")]
+        public int TotalCount { get; set; }
+    }
+}
