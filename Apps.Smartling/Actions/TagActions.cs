4084200f5c007b17cfead0d2d89c8abf6c089e57
ce-nist <cecilia.nistal@blackbird.io>
2025-11-04 17:11:08 -0300

    Check for errors when adding tags to strings

--- Apps.Smartling/Actions/TagActions.cs
+++ Apps.Smartling/Actions/TagActions.cs
@@ -3,7 +3,10 @@ using Apps.Smartling.Models.Identifiers;
 using Apps.Smartling.Models.Requests.Tags;
 using Blackbird.Applications.Sdk.Common;
 using Blackbird.Applications.Sdk.Common.Actions;
+using Blackbird.Applications.Sdk.Common.Exceptions;
 using Blackbird.Applications.Sdk.Common.Invocation;
+using DocumentFormat.OpenXml.Office2016.Excel;
+using Newtonsoft.Json.Linq;
 using RestSharp;
 
 namespace Apps.Smartling.Actions;
@@ -14,12 +17,22 @@ public class TagActions(InvocationContext invocationContext) : SmartlingInvocabl
     [Action("Add tags to strings by hashcode", Description = "Add any amount of tags to any strings.")]
     public async Task AddTagsToStrings([ActionParameter] StringHashcodesIdentifier strings, [ActionParameter] AddTagsRequest input)
     {
+
         var request = new SmartlingRequest($"/tags-api/v2/projects/{ProjectId}/strings/tags/add", Method.Post);
         request.AddJsonBody(new
         {
             tags = input.Tags,
             stringHashCodes = strings.Hashcodes,
         });
-        await Client.ExecuteWithErrorHandling(request);
+        var response = await Client.ExecuteWithErrorHandling(request);
+
+        var json = JObject.Parse(response.Content);
+        var errors = json["response"]?["errors"]?.ToObject<List<JToken>>();
+
+        if (errors != null && errors.Any())
+        {
+            var errorMessages = string.Join("; ", errors.Select(e => e.ToString()));
+            throw new PluginApplicationException(errorMessages);
+        }
     }
 }
