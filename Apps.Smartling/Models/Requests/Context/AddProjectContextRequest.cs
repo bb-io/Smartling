186a7408dd446c1dcf3ae153407ad64b53810596
vitalii-bezuhlyi <vitalii.bezuhlyi@blackbird.io>
2026-06-08 13:32:10 +0300

    Added 'Job ID', 'String hashcodes', 'Content file URI' and 'Override context older than (days)' inputs to 'Upload new context' action

--- Apps.Smartling/Models/Requests/Context/AddProjectContextRequest.cs
+++ Apps.Smartling/Models/Requests/Context/AddProjectContextRequest.cs
@@ -1,4 +1,6 @@
-﻿using Blackbird.Applications.Sdk.Common;
+﻿using Apps.Smartling.DataSourceHandlers;
+using Blackbird.Applications.Sdk.Common;
+using Blackbird.Applications.Sdk.Common.Dynamic;
 using Blackbird.Applications.Sdk.Common.Files;
 
 namespace Apps.Smartling.Models.Requests.Context;
@@ -12,4 +14,17 @@ public class AddProjectContextRequest
 
     [Display("Run automatic context matching")]
     public bool? ContextMatching { get; set; }
+
+    [Display("Job ID")]
+    [DataSource(typeof(JobDataSourceHandler))]
+    public string? TranslationJobUid { get; set; }
+
+    [Display("String hashcodes")]
+    public IEnumerable<string>? StringHashcodes { get; set; }
+
+    [Display("Content file URI")]
+    public string? ContentFileUri { get; set; }
+
+    [Display("Override context older than (days)")]
+    public int? OverrideContextOlderThanDays { get; set; }
 }
