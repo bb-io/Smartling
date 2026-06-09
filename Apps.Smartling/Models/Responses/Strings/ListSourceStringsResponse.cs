ef1a99916776707773443c74e9c3f41c23c8897f
ce-nistal <cecilia.nistal@blackbird.io>
2026-06-09 19:21:16 -0300

    Change response class

--- Apps.Smartling/Models/Responses/Strings/ListSourceStringsResponse.cs
+++ Apps.Smartling/Models/Responses/Strings/ListSourceStringsResponse.cs
@@ -3,9 +3,16 @@ using Blackbird.Applications.Sdk.Common;
 
 namespace Apps.Smartling.Models.Responses.Strings;
 
-public record ListSourceStringsResponse(
-    IEnumerable<SourceStringDto> Strings,
+public class ListSourceStringsResponse
+{
+    public IEnumerable<SourceStringDto> Strings { get; set; }
 
     [Display("String hashcodes")]
-    IEnumerable<string> StringHashcodes
-);
+    public IEnumerable<string> StringHashcodes { get; set; }
+
+    public ListSourceStringsResponse(IEnumerable<SourceStringDto> strings)
+    {
+        Strings = strings;
+        StringHashcodes = strings.Select(x => x.Hashcode).ToList();
+    }
+}
